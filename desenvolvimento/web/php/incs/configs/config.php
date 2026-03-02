2c3bdc147bc101edf54db7e2b5454c7927c105ac
Pedro Miguel <opedromiguel.sc@gmail.com>
2026-03-02 18:58:25 -0300

    Add files via upload

---
+++ desenvolvimento/web/php/incs/configs/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    define("HOST", "127.0.0.1");
+    define("BANCO_DE_DADOS", "siteCO");
+    define("USUARIO_DB", "adminsiteCO");
+    define("SENHA_DB", "1QAZXSW23EDCvcxzasdfrewq1234");
+
+?>
